bbd04a05060c906fd02edccab107c904efc4fde4
SushantDahal <SushanDahal246@gmail.com>
2024-03-31 22:08:13 +0545

    some changes

--- ecommerce_website/backend/server.js
+++ ecommerce_website/backend/server.js
@@ -19,27 +19,27 @@ connection.connect();
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
 
-// // Routes
-// app.post("/register", (req, res) => {
-//   const { firstName, lastName, email, password } = req.body;
+// Routes
+app.post("/register", (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
 
-//   const sql = `INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)`;
-//   const values = [firstName, lastName, email, password];
+  const sql = `INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)`;
+  const values = [firstName, lastName, email, password];
 
-//   connection.query(sql, values, (err, result) => {
-//     if (err) {
-//       console.error("Error registering user:", err);
-//       res
-//         .status(500)
-//         .json({ error: "An error occurred while registering user" });
-//       return;
-//     }
-//     console.log("User registered successfully");
-//     res.status(200).json({ message: "User registered successfully" });
-//   });
-// });
+  connection.query(sql, values, (err, result) => {
+    if (err) {
+      console.error("Error registering user:", err);
+      res
+        .status(500)
+        .json({ error: "An error occurred while registering user" });
+      return;
+    }
+    console.log("User registered successfully");
+    res.status(200).json({ message: "User registered successfully" });
+  });
+});
 
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server is listening at http://localhost:${port}`);
-// });
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
